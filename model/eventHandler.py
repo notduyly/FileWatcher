b243fe326343a4231b234eefff6831fcdf7973bf
notduyly <162235323+notduyly@users.noreply.github.com>
2025-04-30 00:26:46 -0700

    refactor: MyEventHandler and FileWatcher are its own class

---
+++ model/eventHandler.py
@@ -0,0 +1,19 @@
+import logging
+from watchdog.events import FileSystemEventHandler
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        logging.info(f'Modified file at: {event.src_path}')
+        return super().on_modified(event)
+    
+    def on_created(self, event):
+        logging.info(f'Created file at: {event.src_path}')
+        return super().on_created(event)
+    
+    def on_deleted(self, event):
+        logging.info(f'Deleted file at {event.src_path}')
+        return super().on_deleted(event)
---
+++ model/fileWatcher.py
@@ -0,0 +1,29 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+from eventHandler import MyEventHandler
+
+class FileWatcher:
+    def __init__(self, path, event_handler=LoggingEventHandler, recursive=True):
+        self.path = path
+        self.event_handler = event_handler or MyEventHandler()
+        self.recursive = recursive
+        self.observer = None
+    
+    def start(self):
+        self.observer = Observer()
+        self.observer.schedule(self.event_handler, self.path, recursive=self.recursive)
+        self.observer.start()
+        
+    def stop(self):
+        if self.observer:
+            self.observer.stop()
+            self.observer.join()
+    
+    def run(self):
+        try:
+            self.start()
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop()
