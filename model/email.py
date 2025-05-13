4ec35c2e3615d9fb218c3b7e3727a4e08a4623b4
Sungmin Cha <css64534@gmail.com>
2025-05-13 13:07:50 -0700

    smtplib email api

--- model/email.py
+++ model/email.py
@@ -1,54 +1,35 @@
-import base64
+import smtplib
+import ssl
 from email.message import EmailMessage
+import os
 
-import google.auth
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-
-def gmail_create_draft():
-  """Create and insert a draft email.
-  Print the returned draft's message and id.
-  Returns: Draft object, including draft id and message meta data.
-
-  Load pre-authorized user credentials from the environment.
-  TODO(developer) - See https://developers.google.com/identity
-  for guides on implementing OAuth2 for the application.
-  """
-  creds, _ = google.auth.default()
-
-  try:
-    # create gmail api client
-    service = build("gmail", "v1", credentials=creds)
-
-    message = EmailMessage()
-
-    message.set_content("This is automated draft mail")
-
-    message["To"] = "gduser1@workspacesamples.dev"
-    message["From"] = "gduser2@workspacesamples.dev"
-    message["Subject"] = "Automated draft"
-
-    # encoded message
-    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-    create_message = {"message": {"raw": encoded_message}}
-    # pylint: disable=E1101
-    draft = (
-        service.users()
-        .drafts()
-        .create(userId="me", body=create_message)
-        .execute()
-    )
-
-    print(f'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
-
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    draft = None
-
-  return draft
-
-
-if __name__ == "__main__":
-  gmail_create_draft()
+class EmailSender:
+    def __init__(self, sender_email: str, password: str):
+        self.sender_email = sender_email
+        self.password = password
+        self.smtp_server = "smtp.office365.com"
+        self.smtp_port = 587
+        
+    def send_email_with_attachment(self, recipient_email: str, subject: str, body: str, attachment_path: str):
+      
+        message = EmailMessage()
+        message["From"] = self.sender_email
+        message["To"] = recipient_email
+        message["Subject"] = subject
+        message.set_content(body)
+        
+        with open(attachment_path, "rb") as f:
+            file_data = f.read()
+            file_name = os.path.basename(attachment_path)
+        message.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
+        
+        context = ssl.create_default_context()
+        try:
+            with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+                server.starttls(context=context)
+                server.login(self.sender_email, self.password)
+                server.send_message(message)
+                print("📨 Email sent successfully!")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+            
