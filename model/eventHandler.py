33bd2c524958d87acc54dac1b9510e592ad3499a
Sungmin Cha <css64534@gmail.com>
2025-05-14 22:16:44 -0700

    Update eventHandler.py

--- model/eventHandler.py
+++ model/eventHandler.py
@@ -1,39 +1,31 @@
 import logging
 from watchdog.events import FileSystemEventHandler
-from .db_handler import insert_event
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
+# Inherits from FileSystemEventHandler. We override these
+# methods to handle how we react to file changes.
 class MyEventHandler(FileSystemEventHandler):
     def __init__(self, logToTextbox=None):
         super().__init__()
         self.logToTextbox = logToTextbox
     
     def on_modified(self, event):
-        msg = f'Modified file at: {event.src_path}'
+        msg = f'Modified {event.src_path}'
         if self.logToTextbox:
             self.logToTextbox(msg)
-            
-        # insert_event("Modified", event.src_path, is_directory=event.is_directory)
-        
         return super().on_modified(event)
     
     def on_created(self, event):
-        msg = f'Created file at: {event.src_path}'
+        msg = f'Created {event.src_path}'
         if self.logToTextbox:
             self.logToTextbox(msg)
-        
-        # insert_event("Created", event.src_path, is_directory=event.is_directory)
-        
         return super().on_created(event)
     
     def on_deleted(self, event):
-        msg = f'Deleted file at {event.src_path}'
+        msg = f'Deleted {event.src_path}'
         if self.logToTextbox:
             self.logToTextbox(msg)
-            
-        # insert_event("Deleted", event.src_path, is_directory=event.is_directory)
-        
         return super().on_deleted(event)
